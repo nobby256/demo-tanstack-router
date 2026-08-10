05bc4ffcfb4ed10747d33362cc557c1d1c32ce65
nobby256 <helloworld_1024@yahoo.co.jp>
2026-08-10 19:55:34 +0900

    更新

--- apps/react-app/src/pages/crud/error-handling/action.ts
+++ apps/react-app/src/pages/crud/error-handling/action.ts
@@ -1,11 +1,7 @@
 import { useRouter } from '@tanstack/react-router'
 import { useActionBoundary } from '@vendor/router-enhancer'
 
-import {
-  operation,
-  type PageFormOutputValues,
-  type UsePageFormReturn,
-} from './-page-deps-internal'
+import { operation, type UsePageFormReturn } from './-page-deps-internal'
 
 // ─────────────────────────────────────
 // Action Context
@@ -25,7 +21,7 @@ export const useActions = useActionBoundary(({ form }: ActionContext) => {
   const done = async (value: string) => {
     form.setValue('status', value)
 
-    await form.handleSubmit(async (data: PageFormOutputValues) => {
+    await form.handleSubmit(async (data) => {
       await operation.errorHandlingPageDone(data)
 
       // URLを変えずにloaderの再実行
